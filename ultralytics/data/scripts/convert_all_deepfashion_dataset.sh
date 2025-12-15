0106247af931727f2857000d68fa453b502b8191
UltralyticsAssistant <web@ultralytics.com>
2025-12-15 04:23:54 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/data/scripts/convert_all_deepfashion_dataset.sh
+++ ultralytics/data/scripts/convert_all_deepfashion_dataset.sh
@@ -1,4 +1,4 @@
-bash ultralytics/data/scripts/get_consumer_to_shop_Clothes_Retrieval_Benchmark.sh  && python ultralytics/convert_deepfashion.py \
+bash ultralytics/data/scripts/get_consumer_to_shop_Clothes_Retrieval_Benchmark.sh && python ultralytics/convert_deepfashion.py \
   --dataset-root "/root/SmartScan/datasets/consumer_to_shop_Clothes_Retrieval_Benchmark" \
   --images-root "/root/SmartScan/datasets/img" \
   --save-dir "/root/SmartScan/datasets/deepfashion_yolo"
